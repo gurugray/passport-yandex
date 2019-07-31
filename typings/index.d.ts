4039a66a4b86b467ecc945054fbf2475edd74eec
Ivan Zuev <i-zuev@yandex-team.ru>
2019-07-31 16:23:48 +0300

    Added typings

---
+++ typings/index.d.ts
@@ -0,0 +1,28 @@
+import {Strategy as BaseStrategy} from 'passport';
+
+export interface Profile {
+    id: string;
+    provider: string;
+    displayName?: string;
+    emails?: {value: string}[];
+    gender?: string;
+    username?: string;
+    [key: string]: any
+}
+
+interface Config {
+    clientID: string;
+    clientSecret: string;
+    callbackURL: string;
+}
+
+type Callback<U> = (
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (error: string | null, user: U) => void
+) => void;
+
+export class Strategy<U> extends BaseStrategy {
+    public constructor(config: Config, callback: Callback<U>);
+}
